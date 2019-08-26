2eb43ecc4891cacae6d94260549f243c0627f781
dmichalopoulos_komply <dennis@komply.ai>
2019-08-26 12:02:55 -0500

    Refactoring to use centralized cluster management operations in cluster_ops.py.

--- python-clusters/emr-attach-to-existing-cluster/cluster.py
+++ python-clusters/emr-attach-to-existing-cluster/cluster.py
@@ -1,28 +1,25 @@
-import boto3
-import os, json, logging
-import dku_emr
+import logging
 from dataiku.cluster import Cluster
+from cluster_ops import ClusterAttacher, ClusterStopper
 
 # This actually belongs in the main entry point
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.INFO)
 logging.getLogger().setLevel(logging.INFO)
 
+
 class MyCluster(Cluster):
+
     def __init__(self, cluster_id, cluster_name, config, plugin_config):
         self.cluster_id = cluster_id
         self.cluster_name = cluster_name
         self.config = config
         self.plugin_config = plugin_config
-       
+
     def start(self):
-        region_name = self.config.get("awsRegionId") or dku_emr.get_current_region()
-        client = boto3.client("emr", region_name=region_name)
-        clusterId = self.config["emrClusterId"]
-        logging.info("Attaching to EMR cluster id %s" % clusterId)
-        return dku_emr.make_cluster_keys_and_data(client, clusterId, create_user_dir=True)
+        return ClusterAttacher(my_cluster=self).attach_cluster()
 
     def stop(self, data):
         """
         Since we attached to an existing cluster, we don't stop it
         """
-        logging.info("Detaching: nothing to do")
+        return ClusterStopper(my_cluster=self).detach_cluster(data)
